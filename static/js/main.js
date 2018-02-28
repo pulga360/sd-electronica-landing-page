83fb9f73c656c52efd3cc4b166be8af494c7c0ce
HacKan <hackan@gmail.com>
2018-02-27 22:41:18 -0300

    Add dynamic prices

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    $.ajax({
+        url: 'https://raw.githubusercontent.com/pulga360/sd-electronica-landing-page/master/assets/prices.json',
+        type: 'get',
+        dataType: 'json',
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+            $.each(data, function(key, val) {
+                if ($('#' + key).length) {
+                    $('#' + key).html('$ ' + val);
+                }
+            });
+        },
+        statusCode: {
+            404: function() {
+                console.log('There was a problem with the server.  Try again soon!');
+            }
+        }
+    });
+};
